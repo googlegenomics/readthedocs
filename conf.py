03d68bcf2eba9433c049c7cd3e9884125c785e70
cassiedoll <cassiedoll@users.noreply.github.com>
2014-05-02 11:01:25 -0700

    Update copyright

--- conf.py
+++ conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Google Genomics'
-copyright = u'2014, Cassie'
+copyright = u'2014, Google Inc'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
