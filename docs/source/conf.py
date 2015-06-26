8d17f79ea2f253010ad6ceea3c0309f00175ce94
Nicole Deflaux <deflaux@google.com>
2015-06-26 15:01:02 -0700

    Quiet sphinx warnings about includes files.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -67,7 +67,7 @@ release = 'v1beta2'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['_build', 'includes/*']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
